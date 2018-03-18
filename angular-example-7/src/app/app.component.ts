cd185b146baba9e39b8090fa4d022118a91362d9
keep_wan <whevether@outlook.com>
2018-03-18 20:48:39 +0800

    update

--- angular-example-7/src/app/app.component.ts
+++ angular-example-7/src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('f') singupForm:NgForm;
+  defaultQuestion: string = '万文峰';
+  genders:string[] = ['wna','lan'];
+  user:{username:string,email:string,secretQuestion:string,answer:string,gender:string} = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted:boolean = false;
+  suggestUserName(){
+    const suggestedName:string = 'keep-wna';
+    // this.singupForm.setValue({
+    //   userData:{
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret:'万文峰',
+    //   questionAnswer: '',
+    //   gender: ''
+    // });
+    this.singupForm.form.patchValue({
+      userData:{
+        username:suggestedName
+      }
+    });
+  }
+  onSubmit(){
+    this.submitted = true;
+    this.user.username = this.singupForm.value.userData.username;
+    this.user.email = this.singupForm.value.userData.email;
+    this.user.secretQuestion = this.singupForm.value.secret;
+    this.user.answer = this.singupForm.value.questionAnswer;
+    this.user.gender = this.singupForm.value.gender;
+    
+    this.singupForm.reset();
+  }
+  // onSubmit(form:ElementRef){
+  //   console.log(form);
+  // }
+
 }
