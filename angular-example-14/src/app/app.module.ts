fea0af4efe341625da3b387d84d91f0bbb5cd2dc
keep_wan <whevether@outlook.com>
2018-03-21 10:54:08 +0800

    update

--- angular-example-14/src/app/app.module.ts
+++ angular-example-14/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpModule} from '@angular/http';
-// 通过代码操做form 表单
-import {ReactiveFormsModule} from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
 
 
@@ -11,9 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpModule,
-    ReactiveFormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
